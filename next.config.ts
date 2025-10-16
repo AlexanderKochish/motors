db2381a6a882dcf8df6f0cfe2567ea8f24cd48e1
Alexahder Kochish <shvepsolek@gmail.com>
2025-10-16 20:51:15 +0100

    add: vercel.json

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   output: 'standalone', 
   images: {
     remotePatterns: [new URL("https://catalog-management.s3.ap-south-1.amazonaws.com/**")],
     qualities: [75, 85, 100],
